ec175b90449bd1110ca0f446e9721959be078dac
Leah <leah29726@gmail.com>
2019-03-19 20:56:45 -0400

    Complete

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,39 +1,73 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
  */
 public class HamletParser {
-
+    //    Hamlet = 472
+//    Horatio = 158
     private String hamletData;
 
-    public HamletParser(){
+    public HamletParser() {
         this.hamletData = loadFile();
     }
 
-    private String loadFile(){
+    private String loadFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
 
-        try(Scanner scanner = new Scanner(file)){
-            while(scanner.hasNextLine()){
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
 
             scanner.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return result.toString();
     }
 
-    public String getHamletData(){
+    public String getHamletData() {
         return hamletData;
     }
 
+    public String changeHoratioToTariq(String name) {
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(name);
+        name = matcher.replaceAll("Tariq");
+        return name;
+    }
+
+    public String changeHamletToLeon(String name) {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(name);
+        name = matcher.replaceAll("Leon");
+        return name;
+    }
+
+    public Boolean findHoratio(String name) {
+        Pattern myPattern = Pattern.compile("Horatio");
+        Matcher m = myPattern.matcher(name);
+        if (m.find())
+            return true;
+        else
+            return false;
+    }
+
+    public Boolean findHamlet(String name) {
+        Pattern myPattern = Pattern.compile("Hamlet");
+        Matcher m = myPattern.matcher(name);
+        if (m.find())
+            return true;
+        else
+            return false;
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,60 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        String expected = "Lovely Leon tiptoed through the tulips.";
+
+        //When
+        String actual = hamletParser.changeHamletToLeon("Lovely Hamlet tiptoed through the tulips.");
+
+        //Then
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //Given
+        HamletParser hamletParser = new HamletParser();
+        String expected = "Petty Tariq dipped in the pool of pettiness.";
+
+        //When
+        String actual = hamletParser.changeHoratioToTariq("Petty Horatio dipped in the pool of pettiness.");
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
     }
 
     @Test
     public void testFindHoratio() {
+        //Given
+        Boolean firstExpect = true;
+        Boolean secondExpect = false;
+
+        //When
+        Boolean trueness = hamletParser.findHoratio("Horatio, HORATIO. Where you at?");
+        Boolean falseness = hamletParser.findHoratio(" Where you at?");
+
+        //Then
+        Assert.assertEquals(firstExpect, trueness);
+        Assert.assertEquals(secondExpect, falseness);
+
     }
 
     @Test
     public void testFindHamlet() {
+        //Given
+        Boolean firstExpect = true;
+        Boolean secondExpect = false;
+
+        //When
+        Boolean trueness = hamletParser.findHamlet("Hamlet, HAMLET. Where you at?");
+        Boolean falseness = hamletParser.findHamlet(" Where you at?");
+
+        //Then
+        Assert.assertEquals(firstExpect, trueness);
+        Assert.assertEquals(secondExpect, falseness);
+
     }
 }
